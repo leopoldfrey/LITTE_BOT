4135ae8949aae5ca45025b73927de1bf4f812a90
leopoldfrey <leopold.frey@lilo.org>
2022-05-07 09:02:21 +0200

    fix start

--- start.bat
+++ start.bat
@@ -1,8 +1,7 @@
-set curdir = %CD%
-cd %curdir%\LitteBotEditor
+cd C:\Users\littl\Documents\LITTE_BOT\LitteBotEditor
 start python3 ./LitteBotEditor.py
 
-cd %curdir%\LitteBotServer
+cd C:\Users\littl\Documents\LITTE_BOT\LitteBotServer
 start python3 ./LitteBotBrain.py
 
 timeout /t 40 /nobreak
